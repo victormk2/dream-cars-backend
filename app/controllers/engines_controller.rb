3efbca03e0cf3b110271bd6d07912af60b5b0213
Victor Kruger <victor.kruger@senior.com.br>
2024-08-27 20:40:47 -0300

    Update engines_controller.rb

--- app/controllers/engines_controller.rb
+++ app/controllers/engines_controller.rb
@@ -1,27 +1,63 @@
 class EnginesController < ApplicationController
+  before_action :set_engine, only: %i[show update delete]
 
   def index
     @engines = Engine.all
+    render json: { engines: serialized_engines }
   end
 
   def show
-    @engine = Engine.find(params[:id])
-    @engine.run
+    render json: { engine: serialized_engine }
   end
 
   def create
     @engine = Engine.new(engine_params)
 
     if @engine.save
-      redirect_to @engine
+      render json: { engine: serialized_engine }, status: :created
     else
-      render :new
+      render json: @engine.errors, status: :unprocessable_entity
     end
+  end
+
+  def update
+    @engine = Engine.find(params[:id])
+
+    if @engine.update(engine_params)
+      render json: { engine: serialized_engine }, status: :ok
+    else
+      render json: @engine.errors, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    @engine = Engine.find(params[:id])
+    @engine.destroy
+
+    render json: nil, status: :no_content
   end
 
   private
 
   def engine_params
-    params.require(:engine).permit(:model, :description, :year)
+    params.require(:engine).permit(:model, :description, :year, :cylinders)
+  end
+
+  def set_engine
+    @engine = Engine.find(params[:id])
+  end
+
+  def serialized_engines
+    ActiveModelSerializers::SerializableResource.new(
+      @engines,
+      each_serializer: Engines::EngineSerializer
+    )
+  end
+
+  def serialized_engine
+    ActiveModelSerializers::SerializableResource.new(
+      @engine,
+      serializer: Engines::EngineSerializer
+    )
   end
 end
